b3464eca9c2ec145969f767b35b585a158da5d18
Brandon Bondig <brandon@bondig.dk>
2022-06-12 00:17:58 +0200

    added enter-key submit to nav-bar securities component

--- src/components/sideBar.vue
+++ src/components/sideBar.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="body">
-    <div class="form-group d-flex flex-column text-center justify-content-center form">
+    <div
+      class="
+        form-group
+        d-flex
+        flex-column
+        text-center
+        justify-content-center
+        form
+      "
+    >
       <p class="m-0">Security</p>
-      <input type="text" placeholder="e.g SPY" class="form-control" v-model="security">
-      <p style="cursor:pointer; font-weight:bold;">+ Add more</p>
+      <input
+        type="text"
+        style="text-transform: uppercase"
+        v-on:keyup.enter="onClickButton"
+        placeholder="e.g SPY"
+        class="form-control"
+        v-model="security"
+      />
+      <p style="cursor: pointer; font-weight: bold">+ Add more</p>
       <button class="btn btn-success" @click="onClickButton">search</button>
     </div>
   </div>
@@ -12,31 +28,31 @@
 <script>
 export default {
   name: "sidebar",
-  data(){
-    return{
-      security:""
-    }
+  data() {
+    return {
+      security: "",
+    };
   },
-  methods:{
+  methods: {
     onClickButton() {
-    this.$emit('changeSecurity', this.security)
-    }
-  }
+      this.$emit("changeSecurity", this.security);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .body {
-  background-color: #D9D9D9;
+  background-color: #d9d9d9;
   height: 100vh;
   width: 350px;
 }
-input{
+input {
   width: 75%;
   margin: auto;
 }
 
-button{
+button {
   width: 75%;
   margin: auto;
 }
