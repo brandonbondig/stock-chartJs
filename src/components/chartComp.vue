774dcfba7a7989b1016de172ce072181cd5accd6
Brandon Bondig <brandon@bondig.dk>
2022-06-12 13:23:50 +0200

    chart update

--- src/components/chartComp.vue
+++ src/components/chartComp.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="body">
     <Line
-    v-if="loaded"
-    :chart-data="chartData"
-    :chart-options="chartOptions"
-    :styles="styles"
-  />
+      v-if="loaded"
+      :chart-data="chartData"
+      :chart-options="chartOptions"
+      :styles="styles"
+    />
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import { Line } from "vue-chartjs";
-
 
 import {
   Chart as ChartJS,
@@ -23,6 +22,7 @@ import {
   LinearScale,
   PointElement,
   CategoryScale,
+  Filler
 } from "chart.js";
 ChartJS.register(
   Title,
@@ -31,24 +31,33 @@ ChartJS.register(
   LineElement,
   LinearScale,
   PointElement,
-  CategoryScale
+  CategoryScale,
+  Filler
 );
 
 export default {
   name: "chart",
-  props: ['security', 'timeseries'],
-  components:{Line},
+  props: ["security", "timeseries"],
+  components: { Line },
   data() {
     return {
       chartOptions: {
-        scales: { //hides the y axis
-            yAxes: [{
-               display: false
-            }]
-         },
-        legend: {
-          display: false,
+        scales: {
+          //hides the y axis
+          y: {
+            grid: {
+              color: "#F4F5F7",
+              borderColor: "white",
+            },
+          },
+          x: {
+            grid: {
+              display: false,
+              borderColor: "black",
+            },
+          },
         },
+
         maintainAspectRatio: false,
       },
       label: [],
@@ -57,34 +66,34 @@ export default {
       chartData: null,
     };
   },
-  computed:{
-    styles () {
-      return{
-        height:"100%",
-        width:"100%"
-      }
-    }
+  computed: {
+    styles() {
+      return {
+        height: "100%",
+        width: "100%",
+      };
+    },
   },
 
   async mounted() {
     this.loaded = false;
 
     //stock api
-    const stock = await axios.get(`https://financialmodelingprep.com/api/v3/historical-price-full/${this.security}?timeseries=${this.timeseries}&apikey=${process.env.VUE_APP_API_KEY}`)
+    const stock = await axios.get(
+      `https://financialmodelingprep.com/api/v3/historical-price-full/${this.security}?timeseries=${this.timeseries}&apikey=${process.env.VUE_APP_API_KEY}`
+    );
 
-    stock.data.historical.forEach((d)=>{
-      this.price.push(d.close)
-      this.label.push(d.date)
-    })
-
+    stock.data.historical.forEach((d) => {
+      this.price.push(d.close);
+      this.label.push(d.date);
+    });
+    
     this.chartData = {
       labels: this.label.reverse(),
       datasets: [
         {
           label: this.security,
           data: this.price.reverse(),
-          backgroundColor: "#000000",
-          fill: false,
           borderColor: "#000000",
           tension: 0.1,
           pointRadius: 0,
@@ -94,7 +103,6 @@ export default {
     };
     this.loaded = true;
   },
-  
 };
 </script>
 
